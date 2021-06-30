0975ef0ec9faab6c934735776e0d55065927bcf3
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-30 16:34:19 +0530

    Added:new class and method to add person into addressbook

--- addressBookEntity.js
+++ addressBookEntity.js
@@ -1,4 +1,12 @@
-class addressBook{
+class Person{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
     constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
         this.firstName=firstName;
         this.lastName=lastName;
@@ -10,51 +18,84 @@ class addressBook{
         this.email=email;
     }
     set firstName(firstName){
-        this.firstName=firstName;
+        let firstNameRegex=RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if(firstNameRegex.test(firstName));
+            this.firstName=firstName;
     }
     get firstName(){
         return this.firstName;
     }
     set lastName(lastName){
-        this.lastName=lastName;
+        let lastNameRegex=RegExp("^[A-Z]{1}[a-z]{3,}");
+        if(lastNameRegex.test(lastName));
+            this.lastName=lastName;
     }
     get lastName(){
         return this.lastName;
     }
     set address(address){
-        this.address=address;
+        let addressRegex=RegExp("^[a-z]{4,}");
+        if(addressRegex.test(address));
+            this.address=address;
     }
     get address(){
         return this.address;
     }
     set city(city){
-        this.city=city;
+        let cityRegex=RegExp("^[a-z]{4,}");
+        if(cityRegex.test(city));
+            this.city=city;
     }
     get city(){
         return this.city;
     }
     set state(state){
-        this.state=state;
+        let stateRegex=RegExp("^[a-z]{4,}");
+        if(stateRegex.test(state));
+            this.state=state;
     }
     get state(){
         return this.state;
     }
     set zip(zip){
-        this.zip=zip;
+        let zipRegex=RegExp("^\d{5}(?:[-\s]\d{4})?$");
+        if(zipRegex.test(zip));
+            this.zip=zip;
     }
     get zip(){
         return this.zip;
     }
     set phoneNumber(phoneNumber){
-        this.phoneNumber=phoneNumber;
+        let phoneNumberRegex=RegExp("^\s(?:\+?(\d{1,3}))?[-.(]*(\d{3})[-.)]*(\d{3})[-.]*(\d{4})(?:*x(\d+))?\s*");
+        if(phoneNumberRegex.test(phoneNumber));
+            this.phoneNumber=phoneNumber;
     }
     get phoneNumber(){
         return this.phoneNumber;
     }
     set email(email){
-        this.email=email;
+        let emailRegex=RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        if(emailRegex.test(email));
+            this.email=email;
     }
     get email(){
         return this.email;
     }
 }
+class AddressBook{
+    addressbookArray=[];
+    addContact(person){
+        this.addressbookArray.push(person);
+    }
+    showContact(){
+        addressbook.addressbookArray.forEach(person=>{
+            console.log(person.firstName,person.lastName,person.address,person.city,person.state,person.zip,person.phoneNumber,person.email);
+        })
+    }
+}
+let addressbook=new AddressBook();
+var person1=new Person("priyanshi","shukla","987hvhj","kanpur","uttarPradesh","208021","8929373728","pri.shukla22082");
+var person2=new Person("ayush","shukla","987hvhj","kanpur","uttarPradesh","208021","8929373728","pri.shukla22082")
+addressbook.addContact(person1);
+addressbook.addContact(person2);
+addressbook.showContact();
